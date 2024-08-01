6085f1ab6ba3b3d4a66cbcea2bc24b2aa484f87b
SaiMukhund <saimukhund2001@gmail.com>
2024-08-01 21:23:24 +0530

    added crud for rooms

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns=[
     path("",views.home,name="home"),
     path("room/<str:pk>/",views.room,name="room"),
+    path("createroom/",views.createRoom,name="createRoom"),
+    path('updateroom/<str:pk>/',views.updateRoom,name="updateRoom"),
+    path('delete-room/<str:pk>/',views.deleteRoom,name="deleteRoom")
 ]
